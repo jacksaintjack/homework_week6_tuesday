ba674caa53ba0a1e1d165485d77117209e697984
Nicholas Glover <jacksaintjack@gmail.com>
2015-11-10 12:44:41 -0600

    added bower_components, .gitignore, and added my collections and models

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
+//vairables
 var App ={};
 App.Models={};
 App.Collections={};
-Apps.View={};
+App.Views={};
+
+//Models and Collections
+App.Models.Reg = Backbone.Model.extend({
+  url:' http://tiny-starburst.herokuapp.com/collections/contactsjacksaintjack'
+});
+
+App.Collections.Regs = Backbone.Collection.extend({
+  url:' http://tiny-starburst.herokuapp.com/collections/contactsjacksaintjack',
+  model: App.Models.Reg
+});
+//Views
+App.Views.Home = Backbone.View.extend({
+  template: _.template($('#homePage').html()),
+
+  render: function(){
+    this.$el.html(this.template());
+  }
+})
+
+//router
+
+App.Router = Backbone.Router.extend({
+  routes:{
+    '': 'home',
+    'records': 'records'
+  },
+  home: function(){
+    var view = new App.Views.Home();
+    view.render();
+    $('#mainArea').html(view.$el);
+  },
+  records: function(){
+
+  }
+});
+
+App.router = new App.Router();
+Backbone.history.start();
